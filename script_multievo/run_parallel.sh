7165651b8543e36d4cb905448f224146ad0ff86a
Yohsuke Murase <yohsuke.murase@gmail.com>
2021-10-27 19:28:19 +0900

    add runner script

--- script_multievo/run_parallel.sh
+++ script_multievo/run_parallel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-$script_dir/../cmake-build-release/main_multi_evo _input.json
+$script_dir/../cmake-build-release/main_multi_evo_parallel _input.json
 export PIPENV_PIPFILE=${script_dir}/Pipfile
 pipenv run python ${script_dir}/plot_timeseries.py
