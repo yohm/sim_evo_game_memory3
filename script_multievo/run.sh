4fabeb0a61ff97eac36e2eff62401366940d05b0
Yohsuke Murase <yohsuke.murase@gmail.com>
2022-06-10 20:28:12 +0900

    load another input.json when argument is given

--- script_multievo/run.sh
+++ script_multievo/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -eux
 
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-$script_dir/../cmake-build-release/main_multi_evo _input.json
+if [ $# -eq 1 ]; then
+  $script_dir/../cmake-build-release/main_multi_evo $@
+else
+  $script_dir/../cmake-build-release/main_multi_evo _input.json
+fi
 export PIPENV_PIPFILE=${script_dir}/Pipfile
 pipenv run python ${script_dir}/plot_timeseries.py
