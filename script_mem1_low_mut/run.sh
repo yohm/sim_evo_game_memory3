6803656bf5d329508a56546aa13f7b91a449c828
Yohsuke Murase <yohsuke.murase@gmail.com>
2022-09-01 09:34:33 +0900

    removed obsolete files

--- script_mem1_low_mut/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -eux
-
-script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-$script_dir/../cmake-build-release/main_mem1_low_mut $*
-export PIPENV_PIPFILE=${script_dir}/Pipfile
-pipenv run python ${script_dir}/plot_abundance.py
