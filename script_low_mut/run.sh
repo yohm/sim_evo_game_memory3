4fae1285dfcb428a36da139dc61692103ef7954a
Yohsuke Murase <yohsuke.murase@gmail.com>
2023-01-08 21:29:12 +0100

    introduced venv to execute pipenv

--- script_low_mut/run.sh
+++ script_low_mut/run.sh
@@ -2,5 +2,6 @@
 
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 $script_dir/../cmake-build-release/main_multi_evo_low_mut _input.json
+source $script_dir/env/bin/activate
 export PIPENV_PIPFILE=${script_dir}/Pipfile
 pipenv run python ${script_dir}/plot_timeseries.py
